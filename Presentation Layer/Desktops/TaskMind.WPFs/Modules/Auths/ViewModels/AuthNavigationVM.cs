bbc4b9cae91080ffe4ebcbede79cd8462dbcbb07
Akai <88361704+toiQS@users.noreply.github.com>
2026-07-12 14:12:25 +0700

    upload files

---
+++ Presentation Layer/Desktops/TaskMind.WPFs/Modules/Auths/ViewModels/AuthNavigationVM.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.AccessControl;
+using System.Text;
+using System.Windows.Input;
+using TaskMind.WPFs.Utilities;
+
+namespace TaskMind.WPFs.Modules.Auths.ViewModels
+{
+    public class AuthNavigationVM : ViewModelBase
+    {
+        private object _authCurrentView;
+        public object AuthCurrentView
+        {
+            get => _authCurrentView;
+            set { _authCurrentView = value; OnPropertyChanged(); }
+        }
+
+        public ICommand LoginCommand { get; set; }
+        public ICommand RegisterCommand {  get; set; }
+        public ICommand ForgotPasswordCommand { get; set; }
+
+        public void Login(object obj) => AuthCurrentView = new LoginVM();
+        public void Register(object obj) => AuthCurrentView = new RegisterVM();
+        public void ForgotPassword(object obj) => AuthCurrentView = new ForgotPasswordVM();
+
+        public AuthNavigationVM()
+        {
+            LoginCommand = new RelayCommand(Login);
+            RegisterCommand = new RelayCommand(Register);
+            ForgotPasswordCommand = new RelayCommand(ForgotPassword);
+
+            AuthCurrentView = new LoginVM();
+        }
+    }
+}
---
+++ Presentation Layer/Desktops/TaskMind.WPFs/Utilities/ViewModelBase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Windows;
+
+namespace TaskMind.WPFs.Utilities
+{
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string propName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+        }
+    }
+}
