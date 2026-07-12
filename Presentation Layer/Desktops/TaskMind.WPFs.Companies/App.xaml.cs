ba070ce0a17a4e7911fc4ebdcfb307589cc2efcf
Akai <88361704+toiQS@users.noreply.github.com>
2026-07-12 09:55:08 +0700

    Add project files.

---
+++ Presentation Layer/Desktops/TaskMind.WPFs.Companies/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace TaskMind.WPFs.Companies
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
