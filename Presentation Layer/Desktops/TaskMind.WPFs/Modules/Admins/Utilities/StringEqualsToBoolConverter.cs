22928e613362bb091212a30bd05ef9fd62390c5a
Akai <88361704+toiQS@users.noreply.github.com>
2026-07-14 20:34:12 +0700

    upload files

---
+++ Presentation Layer/Desktops/TaskMind.WPFs/Modules/Admins/Utilities/StringEqualsToBoolConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace TaskMind.WPFs.Utilities
+{
+    /// <summary>
+    /// So sánh giá trị binding (ActiveKey) với ConverterParameter (key của từng menu item).
+    /// Dùng để bind IsChecked của RadioButton menu, giúp highlight mục đang được chọn.
+    /// </summary>
+    public class StringEqualsToBoolConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null || parameter == null) return false;
+            return string.Equals(value.ToString(), parameter.ToString(), StringComparison.OrdinalIgnoreCase);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            // RadioButton khi được check sẽ trả về true -> trả lại key tương ứng
+            if (value is bool b && b) return parameter?.ToString();
+            return Binding.DoNothing;
+        }
+    }
+}
--- Presentation Layer/Desktops/TaskMind.WPFs/Modules/Admins/ViewModels/AdminNavigationVM.cs
+++ Presentation Layer/Desktops/TaskMind.WPFs/Modules/Admins/ViewModels/AdminNavigationVM.cs
@@ -4,6 +4,7 @@ using System.Configuration.Internal;
 using System.Net.NetworkInformation;
 using System.Runtime.CompilerServices;
 using System.Text;
+using System.Windows;
 using System.Windows.Input;
 using TaskMind.WPFs.Modules.Auths.ViewModels;
 using TaskMind.WPFs.Utilities;
@@ -16,9 +17,16 @@ namespace TaskMind.WPFs.Modules.Admins.ViewModels
         public object AdminCurrentView
         {
             get => _adminCurrentView;
-            set { _adminCurrentView = value;  OnPropertyChanged(); }
+            set { _adminCurrentView = value; OnPropertyChanged(); }
         }
 
+        // Key của menu đang được chọn, dùng để highlight item tương ứng trên sidebar
+        private string _activeKey;
+        public string ActiveKey
+        {
+            get => _activeKey;
+            set { _activeKey = value; OnPropertyChanged(); }
+        }
 
         public ICommand DashbroadCommand { get; set; }
         public ICommand UserCommand { get; set; }
@@ -27,21 +35,47 @@ namespace TaskMind.WPFs.Modules.Admins.ViewModels
         public ICommand SchoolCommand { get; set; }
         public ICommand ProfitCommand { get; set; }
         public ICommand SkillCommand { get; set; }
-        public ICommand ReportCommand {  get; set; }
+        public ICommand ReportCommand { get; set; }
         public ICommand ChatCommand { get; set; }
-        public ICommand HandlerCommand {  get; set; }
+        public ICommand HandlerCommand { get; set; }
 
-        private void Dashbroad(object obj) => AdminCurrentView = new DashbroadVM();
-        private void User(object obj) => AdminCurrentView = new UserVM();
-        private void Profile(object obj) => AdminCurrentView = new ProfileVM();
-        private void Skill(object obj) => AdminCurrentView = new SkillVM();
-        private void Company(object obj) => AdminCurrentView = new CompanyVM();
-        private void School(object obj) => AdminCurrentView = new SchoolVM();
-        private void Profit(object obj) => AdminCurrentView = new ProfitVM();
-        private void Report(object obj) => AdminCurrentView = new ReportVM();
-        private void Handler(object obj) => AdminCurrentView = new HandlerVM();
-        private void Chat(object obj ) => AdminCurrentView = new ChatVM();
+        // Lệnh cho khu vực dưới cùng của sidebar
+        public ICommand LogoutCommand { get; set; }
+        public ICommand ExitCommand { get; set; }
 
+        private void Dashbroad(object obj) { AdminCurrentView = new DashbroadVM(); ActiveKey = "Dashbroad"; }
+        private void User(object obj) { AdminCurrentView = new UserVM(); ActiveKey = "User"; }
+        private void Profile(object obj) { AdminCurrentView = new ProfileVM(); ActiveKey = "Profile"; }
+        private void Skill(object obj) { AdminCurrentView = new SkillVM(); ActiveKey = "Skill"; }
+        private void Company(object obj) { AdminCurrentView = new CompanyVM(); ActiveKey = "Company"; }
+        private void School(object obj) { AdminCurrentView = new SchoolVM(); ActiveKey = "School"; }
+        private void Profit(object obj) { AdminCurrentView = new ProfitVM(); ActiveKey = "Profit"; }
+        private void Report(object obj) { AdminCurrentView = new ReportVM(); ActiveKey = "Report"; }
+        private void Handler(object obj) { AdminCurrentView = new HandlerVM(); ActiveKey = "Handler"; }
+        private void Chat(object obj) { AdminCurrentView = new ChatVM(); ActiveKey = "Chat"; }
+
+        private void Logout(object obj)
+        {
+            // TODO: gọi service đăng xuất (clear token/session) rồi điều hướng về màn hình Login
+            var result = MessageBox.Show("Bạn có chắc muốn đăng xuất?", "Đăng xuất",
+                MessageBoxButton.YesNo, MessageBoxImage.Question);
+
+            if (result == MessageBoxResult.Yes)
+            {
+                // Ví dụ: NavigationService.Instance.NavigateTo(new LoginVM());
+            }
+        }
+
+        private void Exit(object obj)
+        {
+            var result = MessageBox.Show("Bạn có chắc muốn thoát ứng dụng?", "Thoát",
+                MessageBoxButton.YesNo, MessageBoxImage.Warning);
+
+            if (result == MessageBoxResult.Yes)
+            {
+                Application.Current.Shutdown();
+            }
+        }
 
         public AdminNavigationVM()
         {
@@ -55,7 +89,12 @@ namespace TaskMind.WPFs.Modules.Admins.ViewModels
             ReportCommand = new RelayCommand(Report);
             HandlerCommand = new RelayCommand(Handler);
             ChatCommand = new RelayCommand(Chat);
+
+            LogoutCommand = new RelayCommand(Logout);
+            ExitCommand = new RelayCommand(Exit);
+
             AdminCurrentView = new DashbroadVM();
+            ActiveKey = "Dashbroad";
         }
     }
 }
