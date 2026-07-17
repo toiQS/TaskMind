3e50ed739256c1f61df10368146e6c98e843ea29
Akai <88361704+toiQS@users.noreply.github.com>
2026-07-17 11:43:29 +0700

    upload file error

--- Presentation Layer/Desktops/TaskMind.WPFs/Modules/Companies/Views/DashbroadView.xaml.cs
+++ Presentation Layer/Desktops/TaskMind.WPFs/Modules/Companies/Views/DashbroadView.xaml.cs
@@ -1,26 +1,72 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using TaskMind.WPFs.Modules.Companies.Models;
+using TaskMind.WPFs.Modules.Companies.ViewModels;
 
 namespace TaskMind.WPFs.Modules.Companies.Views
 {
-    /// <summary>
-    /// Interaction logic for DashbroadView.xaml
-    /// </summary>
     public partial class DashbroadView : UserControl
     {
         public DashbroadView()
         {
             InitializeComponent();
+            DataContextChanged += DashbroadView_DataContextChanged;
+            Loaded += (_, _) => DrawChart();
+        }
+
+        private void DashbroadView_DataContextChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if (e.OldValue is DashbroadVM oldVm)
+                oldVm.PropertyChanged -= Vm_PropertyChanged;
+
+            if (e.NewValue is DashbroadVM newVm)
+                newVm.PropertyChanged += Vm_PropertyChanged;
+
+            DrawChart();
+        }
+
+        private void Vm_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            // ProjectProgress là ObservableCollection nên không raise PropertyChanged khi Add/Clear từng item,
+            // nhưng ta gọi lại DrawChart mỗi khi IsBusy chuyển từ true -> false (load xong dữ liệu).
+            if (e.PropertyName == nameof(DashbroadVM.IsBusy))
+                DrawChart();
+        }
+
+        private void ChartHost_SizeChanged(object sender, SizeChangedEventArgs e) => DrawChart();
+
+        private void DrawChart()
+        {
+            if (DataContext is not DashbroadVM vm) return;
+            if (ChartHost.ActualWidth <= 0 || ChartHost.ActualHeight <= 0) return;
+
+            var points = vm.ProjectProgress?.ToList();
+            if (points == null || points.Count == 0)
+            {
+                ProgressLine.Points = null;
+                return;
+            }
+
+            double width = ChartHost.ActualWidth;
+            double height = ChartHost.ActualHeight;
+
+            double max = points.Max(p => p.Value);
+            double min = points.Min(p => p.Value);
+            double range = max - min == 0 ? 1 : max - min;
+            double stepX = points.Count == 1 ? 0 : width / (points.Count - 1);
+
+            var result = new PointCollection();
+            for (int i = 0; i < points.Count; i++)
+            {
+                double x = i * stepX;
+                // padding trên/dưới 10% để đường không dính sát mép
+                double y = height * 0.9 - ((points[i].Value - min) / range * height * 0.8);
+                result.Add(new Point(x, y));
+            }
+
+            ProgressLine.Points = result;
         }
     }
 }
