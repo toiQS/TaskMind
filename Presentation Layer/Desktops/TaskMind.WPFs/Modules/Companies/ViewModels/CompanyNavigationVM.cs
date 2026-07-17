bef016984463c2050c341aa0df5f33ef48f0c462
Akai <88361704+toiQS@users.noreply.github.com>
2026-07-17 10:21:50 +0700

    skeleten company role

---
+++ Presentation Layer/Desktops/TaskMind.WPFs/Modules/Companies/ViewModels/CompanyNavigationVM.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TaskMind.WPFs.Utilities;
+
+namespace TaskMind.WPFs.Modules.Companies.ViewModels
+{
+    internal class CompanyNavigationVM : ViewModelBase
+    {
+    }
+}
---
+++ Presentation Layer/Desktops/TaskMind.WPFs/Modules/Companies/ViewModels/DashbroadVM.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskMind.WPFs.Modules.Companies.ViewModels
+{
+    internal class DashbroadVM
+    {
+    }
+}
