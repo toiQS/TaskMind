c97fa05b9046f5f7508db504db0f49fe4712db22
Akai <88361704+toiQS@users.noreply.github.com>
2026-07-20 00:22:47 +0700

    upload files

--- Presentation Layer/Desktops/TaskMind.WPFs/Modules/Companies/Views/CompanyPage.xaml.cs
+++ Presentation Layer/Desktops/TaskMind.WPFs/Modules/Companies/Views/CompanyPage.xaml.cs
@@ -22,5 +22,18 @@ namespace TaskMind.WPFs.Modules.Companies.Views
         {
             InitializeComponent();
         }
+
+        /// <summary>Xác nhận trước khi thoát khỏi toàn bộ ứng dụng.</summary>
+        private void ExitApp_Click(object sender, RoutedEventArgs e)
+        {
+            var result = MessageBox.Show(
+                "Bạn có chắc chắn muốn thoát khỏi hệ thống?",
+                "Xác nhận thoát",
+                MessageBoxButton.YesNo,
+                MessageBoxImage.Question);
+
+            if (result == MessageBoxResult.Yes)
+                Application.Current.Shutdown();
+        }
     }
 }
