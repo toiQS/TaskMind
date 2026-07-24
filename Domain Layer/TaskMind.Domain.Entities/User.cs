a34de5ada6deeb1c8d23700dacc568780a8c2758
Akai <88361704+toiQS@users.noreply.github.com>
2026-07-24 11:55:56 +0700

    Update User.cs

--- Domain Layer/TaskMind.Domain.Entities/User.cs
+++ Domain Layer/TaskMind.Domain.Entities/User.cs
@@ -9,7 +9,10 @@ namespace TaskMind.Domain.Entities
     /// <summary>
     /// user là tài khoản khởi đầu của mọi người dùng trong hệ thông (student, teacher, staff), không bao gồm (admin company, admin school, admin system). 
     /// User có thể đăng nhập vào hệ thống và thực hiện các chức năng cơ bản.
-    /// User dùng như cơ ở dữ liệu đối chiếu thông tin ban đầu và lưu trữ thông tin cơ bản và là công cụ ghi nhận xác minh kinh nghiệm.
+    /// User dùng như cơ ở dữ liệu đối chiếu thông tin ban đầu và lưu trữ thông tin cơ bản và là công cụ ghi nhận xác minh kinh nghiệm và kỹ năng.
+    /// nếu người dùng có nhu cầu tăng level kỹ năng cần có sự đảm bảo từ người có quyền hạn cao hơn trong đơn vị làm việc để gửi yêu cầu nâng level kỹ năng, và người có quyền hạn cao hơn sẽ xác minh kinh nghiệm và kỹ năng của user để nâng level kỹ năng.
+    /// hoặc sẽ thực hiện chu trình đánh giá năng lực theo quy trình hệ thống trước khi uplevel kỹ năng.
+    /// cảnh báo nếu trong quá trình nâng kỹ năng nếu người dùng không có kinh nghiệm thực tế hoặc không được xác minh thì sẽ bị hạ level kỹ năng và bị cảnh báo. (x2 hình phạt như một lời cảnh báo)
     /// nếu giam nhập thông tin hoặc cơ sở đào tạo thì cấp tài khoản mới với role tương ứng và có liên kết trỏ đến đúng người dùng để truy cập thông tin cơ bản (yêu cầu xác minh trước)
     /// lịch sử tham gia dự án và kinh nghiệm sẽ được lưu trữ trong user và có thể truy cập thông qua các tài khoản khác nhau (student, teacher, staff) để xác minh kinh nghiệm.
     /// user có nguyền đăng ký thành lập công ty hoặc cơ sở đào tạo, nhưng phải được xác minh thông tin trước khi cấp quyền quản lý công ty hoặc cơ sở đào tạo. (yêu cầu xác minh trước)
